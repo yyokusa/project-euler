7ac56f22d7db52e5f1629526212a069384fceabe
Yasin A. Yokus <ayokus@icloud.com>
2023-06-28 00:01:37 +0300

    solve even fibonacci

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,20 @@ pub mod multiples {
         println!("result: {:?}", result);
     }
 }
+
+pub mod fibonacci {
+    fn fibonacci(n: u32, memo: &mut Vec<u64>) -> u64 {
+        let mut i = 2;
+        while memo[memo.len() - 1] < n as u64 {
+            memo.push(memo[i as usize - 1] + memo[i as usize - 2]);
+            i += 1;
+        }
+        // sum evens
+        return memo.iter().filter(|&x| x % 2 == 0).sum();
+    }
+    pub fn solve_problem_02() {
+        let mut memo: Vec<u64> = vec![1, 2];
+        let result = fibonacci(4_000_000, &mut memo);
+        println!("result: {:?}", result);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use euler_problems::multiples::solve_problem_01;
+// use euler_problems::multiples::solve_problem_01;
+use euler_problems::fibonacci::solve_problem_02;
 
 fn main() {
-    solve_problem_01();
+    // solve_problem_01();
+    solve_problem_02();
 }
